26c4c6bed9bc68d2ac9ed9e9de1fbff51e09b9c6
Toni <tonitorres.dev@gmail.com>
2020-09-08 11:31:23 +0200

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
+import { MyPanelComponent } from './components/my-panel/my-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CreateNoteComponent,
+    EditNoteComponent,
+    MyPanelComponent
   ],
   imports: [
     BrowserModule,
