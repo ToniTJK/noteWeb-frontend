eb0a72f3aad4111b5f400444614fd405f5e651d4
Toni <tonitorres.dev@gmail.com>
2020-09-08 11:41:30 +0200

    error-page and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/* COMPONENTS */
+import { HomeComponent } from './components/home/home.component';
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
+import { MyPanelComponent } from './components/my-panel/my-panel.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'create-note', component: CreateNoteComponent },
+  { path: 'edit-note', component: EditNoteComponent },
+  { path: 'my-panel', component: MyPanelComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/* COMPONENTS */
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { CreateNoteComponent } from './components/create-note/create-note.component';
 import { EditNoteComponent } from './components/edit-note/edit-note.component';
 import { MyPanelComponent } from './components/my-panel/my-panel.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { MyPanelComponent } from './components/my-panel/my-panel.component';
     HomeComponent,
     CreateNoteComponent,
     EditNoteComponent,
-    MyPanelComponent
+    MyPanelComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
